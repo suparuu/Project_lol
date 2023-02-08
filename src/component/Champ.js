032c3f56161cfa010bb825ffaf58469bb94947f1
suparuu <kkwoo97@naver.com>
2023-02-08 18:35:41 +0900

    02-08

--- src/component/Champ.js
+++ src/component/Champ.js
@@ -1,20 +1,20 @@
 import { useState, useRef, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import "../css/Champ.scss";
-import $ from 'jquery'
+import $ from "jquery";
 
 const Champ = () => {
   let name = useParams();
   const dataRef = useRef();
   const Bigimg = useRef(); //큰 챔피언 사진
-  const cmpEnname = useRef();//영어이름
-  const cmpKrname = useRef();//한글이름
+  const cmpEnname = useRef(); //영어이름
+  const cmpKrname = useRef(); //한글이름
   const [qwer, setQwer] = useState(); //qwer스킬 데이터
-  const [skilldata, setSkilldata] = useState();//스킬 데이터
-  const [skindata, setSkindata] = useState();//스킨 데이터
-  const [skillclick, setSkillclick] = useState();//스킬클릭
-  const [skinclick, setSkinclick] = useState({ num: 0 });//스킨클릭
-  const testH = useRef();//이미지 높이값
+  const [skilldata, setSkilldata] = useState(); //스킬 데이터
+  const [skindata, setSkindata] = useState(); //스킨 데이터
+  const [skillclick, setSkillclick] = useState(); //스킬클릭
+  const [skinclick, setSkinclick] = useState({ num: 0 }); //스킨클릭
+  const testH = useRef(); //이미지 높이값
 
   const ChampName = name.name;
   const cmpUrl = "https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/"; //챔프 url
@@ -45,12 +45,40 @@ const Champ = () => {
     setSkindata(skindata);
     console.log(skindata, "당장필요한거"); //스킨 담겨있는 배열
     setSkillclick(datas[ChampName].passive);
-}
-useEffect(()=>{
-    document.onreadystatechange =()=>{
-        console.log(testH.current.clientHeight)
-    }
-},[])
+  }
+
+
+  useEffect(() => {
+    document.onreadystatechange = () => {
+      console.log(testH.current.clientHeight);
+    };
+
+    
+    //미디어 쿼리
+    /* function mdq(key){
+      let mq = window.matchMedia("(min-width:390px)");
+      mq.addListener((e) => {
+        if (e.matches) {
+          console.log("pc");
+        } else {
+    const thumb = document.querySelectorAll(".skinshow02 > div");
+    console.log(thumb,'모바일 확인')
+
+          $(".skinshow02").css({
+            transform: `translateY(-${thumb[key].offsetWidth}px)`,
+            transition: "0.5s",
+          });
+        }
+      });
+
+    } */
+
+
+  }, []);
+
+
+
+
   useEffect(() => {
     fetch(
       `https://ddragon.leagueoflegends.com/cdn/13.1.1/data/ko_KR/champion/${ChampName}.json` //누른 챔피언의 상세 api
@@ -71,28 +99,72 @@ useEffect(()=>{
       costBurn: skilldata[num].costBurn,
       rangeBurn: skilldata[num].rangeBurn,
     });
+    $(".textbox01").css({ opacity: "1", transition: "0.5s" });
   }
-  function clickskin(key) {
+
+
+  function mdq(key){
+    let mq = window.matchMedia("(min-width:390px)");
+    mq.addListener((e) => {
+      if (e.matches) {
+        console.log("pc");
+      } else {
+  const thumb = document.querySelectorAll(".skinshow02 > div");
+  console.log(thumb,'모바일 확인')
+
+        $(".skinshow02").css({
+          transform: `translateY(-${thumb[key].offsetWidth}px)`,
+          transition: "0.5s",
+        });
+      }
+    });
+
+  }
+
+  /* function clickskin(key) {
     setSkinclick({ num: skindata[key].num, name: skindata[key].name });
+    const thumb = document.querySelectorAll(".skinshow02 > div");
+    let mq = window.matchMedia("(min-width:390px)");
+    mq.addListener((e)=>{
+      if (e.matches) {
+        console.log('pc',thumb,e)
+        $(".skinshow02").css({
+          transform: `translateY(-${thumb[key].offsetTop}px)`,
+          transition: "0.5s",
+        });
+        $(".skinshow03 img").css({ transition: "0.5s" });
+      }else{
+        
+        console.log('mob')
+        $(".skinshow02").css({
+          transform: `translateY(-${thumb[key].offsetWidth}px)`,
+          transition: "0.5s",
+        });
+      } */
+      function clickskin(key){
+        setSkinclick({ num: skindata[key].num, name: skindata[key].name });
+        const thumb = document.querySelectorAll(".skinshow02 > div");
+
+        $(".skinshow02").css({
+          transform: `translateY(-${thumb[key].offsetTop}px)`,
+          transition: "0.5s",
+        });
+        $(".skinshow03 img").css({ transition: "0.5s" });
+
     console.log(skinclick, "실행되나용");
-  }
-  console.log($('.skinshow01').height())
-  let skinheight = $('.skinshow02').height();//스킨+이름 높이
-  let hiddenH = $('.skinshow03 img').height();//스킨이미지 높이
+      }
+  console.log($(".skinshow01").height());
+  let skinheight = $(".skinshow02").height(); //스킨+이름 높이
+  let hiddenH = $(".skinshow03 img").height(); //스킨이미지 높이
 
-  console.log(skinheight,'asdasdasd')
-  function csstest(){
-    $('.skinshow01').css('transform',`translateY(-${skinheight}px)`).css('transition','1s')
-  }
+  console.log(skinheight, "asdasdasd");
 
-    
   skindata && console.log(skindata);
   return (
     qwer && (
       <>
         <section className="champbox02">
           <div className="cmpimgBack">
-            
             <img
               className="frontimg"
               src={`${Bigimg.current && Bigimg.current}`}
@@ -125,7 +197,11 @@ useEffect(()=>{
               skilldata.map((obj, key) => {
                 //qwer데이터 뿌리기 반복문
                 return (
-                  <div className="skillbox02" onClick={() => clickskill(key)}>
+                  <div
+                    className="skillbox02"
+                    onClick={() => clickskill(key)}
+                    key={key}
+                  >
                     <img src={`${spellUrl}${obj.image.full}`}></img>
                     <p>{obj.name}</p>
                   </div>
@@ -151,25 +227,36 @@ useEffect(()=>{
         </section>
 
         <h2>챔피언 스킨</h2>
-        <section className="skinbox" style={{height:hiddenH}}>
-          <div className="skinshow01" onClick={csstest} >
-            {skindata &&
-              skindata.map((obj, key) => {
-                return (
-                  <div className="skinshow02">
-                    <img
-                      src={`${splashUrl}${cmpEnname.current}_${obj.num}.jpg`}
-                      onClick={() => clickskin(key)}
-                    ></img>
-                    <p>{obj.name.replaceAll("default", cmpKrname.current)}</p>
-                  </div>
-                );
-              })}
-
+        <section className="skinbox" style={{ height: hiddenH }}>
+          <div className="skinshow01">
+            <div className="skinshow02">
+              {skindata &&
+                skindata.map((obj, key) => {
+                  return (
+                    <div key={key}>
+                      <img
+                        src={`${splashUrl}${cmpEnname.current}_${obj.num}.jpg`}
+                        onClick={() => clickskin(key)}
+                        key={key}
+                      ></img>
+                      <p>{obj.name.replaceAll("default", cmpKrname.current)}</p>
+                    </div>
+                  );
+                })}
             </div>
-          <div className="skinshow03">
-            <img testH={testH} src={`${splashUrl}${cmpEnname.current}_${skinclick && skinclick.num}.jpg`}></img>
           </div>
+          <div className="skinshow03">
+            <img
+              testH={testH}
+              src={`${splashUrl}${cmpEnname.current}_${
+                skinclick && skinclick.num
+              }.jpg`}
+            ></img>
+          </div>
+        </section>
+
+        <section className="home">
+          <Link to="/Main">메인으로</Link>
         </section>
       </>
     )
--- src/component/Main.js
+++ src/component/Main.js
@@ -8,8 +8,7 @@ import Tank from "./Tank";
 import Marksman from "./Marksman";
 import Fighter from "./Fighter";
 import Asdf from "./Asdf";
-import axios from 'axios';
-
+import axios from "axios";
 import "../css/Main.scss";
 
 const Main = (props) => {
@@ -26,22 +25,23 @@ const Main = (props) => {
       .catch((err) => console.error(err));
   }, []);
 
-      /* axios.get("https://ddragon.leagueoflegends.com/cdn/13.1.1/data/ko_KR/champion.json")//챔프 전체 데이터api
+  /* axios.get("https://ddragon.leagueoflegends.com/cdn/13.1.1/data/ko_KR/champion.json")//챔프 전체 데이터api
       .then((Response)=>lolMain(Response))
       .catch((error)=>{console.log(error)}) */
 
-
-  const [catestate,setCatestate] = useState();
-  const [test5, setTest5] = useState([]);//챔프 이름,영문,태그명
+  const [catestate, setCatestate] = useState();
+  const [test5, setTest5] = useState([]); //챔프 이름,영문,태그명
   const [aaa, setAaa] = useState();
   const data1 = useRef("0");
   const [role, setRole] = useState();
-//const[t,setT]
+  //const[t,setT]
 
   function lolMain(test) {
     const champ = test.data;
     const ChampUrl =
       "https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/";
+
+    console.log(champ + "dfsdfsdf");
 
     const ChampName = [];
     for (let name in champ) {
@@ -51,21 +51,21 @@ const Main = (props) => {
         tags: champ[name].tags,
       });
     } //영문이름,한글이름
-    console.log(champ,'챔프데이터');
+    // console.log(champ, "챔프데이터");
 
     ChampName.sort(function (a, b) {
       return a.kr < b.kr ? -1 : a.kr > b.kr ? 1 : 0;
     }); //가나다순 함수
     setTest5(ChampName);
     setAaa(champ);
-
+    setCatestate(ChampName);
     const searchSpace = (e) => {
       let keyword = e.target.value;
       this.setText({});
     }; //검색기능 함수
-    console.log(ChampName,'챔프 이름,영문,태그명');
-    console.log(test5)
-   /*  const aaaa = () =>{
+    // console.log(ChampName, "챔프 이름,영문,태그명");
+
+    /*  const aaaa = () =>{
 
         test5&&test5.map((obj)=>{
             obj.tags.map((obj2)=>{
@@ -74,8 +74,7 @@ const Main = (props) => {
         })
     }
     aaaa() */
-    
-    
+
     /* const serachOn=()=>{
         if(text === null || text === ''){
         }
@@ -86,75 +85,159 @@ const Main = (props) => {
         }
     
 } */
+  } //Main
 
-  }//Main
-  const tagname = ["Marksman","Tank","Mage","Fighter","Assassin","Support"]
- 
- 
-  const category = (tags) =>{
-    if(tags == "전체보기"){
-        setCatestate(test5)
-        console.log('전체보기',catestate)
-    }else{
-        setCatestate(test5.filter((d)=>{
-            return d.tags == tags
-        }))
-    }//카테고리 함수
-    
-    console.log(tags)
-    console.log(catestate)
-  }
+  const tagname = [
+    "Marksman",
+    "Tank",
+    "Mage",
+    "Fighter",
+    "Assassin",
+    "Support",
+  ];
 
-  const [text, setText] = useState("");
-  const [asdf,setAsdf]= useState();
+  const category = (tags) => {
+    if (tags == "전체보기") {
+      setCatestate(test5);
+      console.log("전체보기", catestate);
+    } else {
+      setCatestate(
+        test5.filter((d) => {
+          return d.tags.includes(tags);
+        })
+      );
+    } //카테고리 함수
 
-  console.log(text,'채팅')
-  console.log(test5)
+    console.log(tags);
+    console.log(catestate);
+  };
+
+  const [text, setText] = useState("");//input값
+  const text2 = useRef();//버튼 누를때
+
+  const textinput = () =>{
+    setText(text2.current.value)
+    text2.current.focus()
+  };//검색 기능
+
+
+  const textreset = () =>{
+    setText('')
+  };//초기화 버튼기능 
+
+
+  const onKeyPress = (e) =>{
+    if(e.key == 'Enter'){
+      textinput();
+    }
+  };//엔터키 누를시 검색기능
+
+
+ /*  useEffect(() => {
+    console.log(text2.current.value);
+    text2.current.focus();
+  }, []) */
   /* const items = catestate.filter((data)=>{
     data.kr.includes(text)
   })
   setAsdf(items)
   console.log(asdf) */
- /*  catestate.map((obj)=>{
+  /*  catestate.map((obj)=>{
     obj.filter((obj2)=>{
         console.log(obj2)
 
     })
   }) */
+  
 
-  console.log(catestate)
+  console.log(catestate);
 
   return (
     <>
       <main>
         <Routes>
           <Route path="/Champ/:name" element={<Champ />}></Route>
-          <Route path="/Asdf" element={<Asdf test5={test5} setTest5={setTest5} props={props} aaa={aaa} setAaa={setAaa} role={role} setRole={setRole}/>}></Route>
-
+          <Route
+            path="/Asdf"
+            element={
+              <Asdf
+                test5={test5}
+                setTest5={setTest5}
+                props={props}
+                aaa={aaa}
+                setAaa={setAaa}
+                role={role}
+                setRole={setRole}
+              />
+            }
+          ></Route>
         </Routes>
         <section className="champions">
           <div className="headbox">
             <div className="buttonbox">
-              <button onClick={()=>{category("전체보기")}}>전체보기</button>
-              <button onClick={()=>{category("Mage")}}>법사</button>
-              <button onClick={()=>{category("Tank")}}>태ㅔㅇ크 </button>
-              <button onClick={()=>{category("Fighter")}}>브루저</button>
-              <button onClick={()=>{category("Assassin")}}>암살자</button>
-              <button onClick={()=>{category("Support")}}>사퍼타</button>
-              <button onClick={()=>{category("Marksman")}}>원딜</button>
-            </div>
-            <div className="searchbox">
-              <input  value={text} type="text" onChange={(e) => { setText(e.target.value)}}></input>
-              {
-              }
-              <button onClick={''}>검색</button>
+              <button
+                onClick={() => {
+                  category("전체보기");
+                }}
+              >
+                전체보기
+              </button>
+              <button
+                onClick={() => {
+                  category("Mage");
+                }}
+              >
+                마법사
+              </button>
+              <button
+                onClick={() => {
+                  category("Tank");
+                }}
+              >
+                탱커
+              </button>
+              <button
+                onClick={() => {
+                  category("Fighter");
+                }}
+              >
+                브루저
+              </button>
+              <button
+                onClick={() => {
+                  category("Assassin");
+                }}
+              >
+                암살자
+              </button>
+              <button
+                onClick={() => {
+                  category("Support");
+                }}
+              >
+                서포터
+              </button>
+              <button
+                onClick={() => {
+                  category("Marksman");
+                }}
+              >
+                원딜
+              </button>
             </div>
           </div>
+
+            <div className="searchbox">
+              <input onKeyPress={onKeyPress} ref={text2} type="text"></input>
+              <button  onClick={textinput}>검색</button>
+              <button onClick={textreset}>초기화</button>
+            </div>
           <div className="champbox01">
-            {/* {test5 &&
-              test5.map((obj) => {
+            {catestate &&
+              catestate.map((obj, k) => {
+                if (obj.kr == text)
                   return (
-                    <Link to={`/Champ/${obj.en}`}>
+                    <Link to={`/Champ/${obj.en}`} key={k}>
                       <div className="imgbox01">
                         <img
                           src={`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${obj.en}.png`}
@@ -163,29 +246,19 @@ const Main = (props) => {
                       </div>
                     </Link>
                   );
-              })} */}
-            {/* {catestate&&catestate.map((obj)=>{
-                return(
-                    <Link to={`/Champ/${obj.en}`}>
-                    <div className="imgbox01">
-                        <img src={`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${obj.en}.png`}></img>
+                if (text == "") {
+                  return (
+                    <Link to={`/Champ/${obj.en}`} key={k}>
+                      <div className="imgbox01">
+                        <img
+                          src={`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${obj.en}.png`}
+                        ></img>
                         <span>{[obj.kr]}</span>
                       </div>
                     </Link>
-
-                )
-            })} */}
-            {catestate&&catestate.map((obj)=>{
-                return(
-                    <Link to={`/Champ/${obj.en}`}>
-                    <div className="imgbox01">
-                        <img src={`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${obj.en}.png`}></img>
-                        <span>{[obj.kr]}</span>
-                      </div>
-                    </Link>
-
-                )
-            })}
+                  );
+                }
+              })}
           </div>
         </section>
       </main>
